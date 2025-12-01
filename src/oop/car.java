0189c92a9735ec3d15acb153b85cd6f14262f834
Mksa2130 <mksa2130@gmail.com>
2025-12-01 14:52:41 +0530

    first commit

--- src/oop/car.java
+++ src/oop/car.java
@@ -1,4 +1,24 @@
 package oop;
 
 public class car {
+    //fields, attribute, properties
+
+    String colour;
+    String brand;
+    int speed;
+
+    public static void main(String[] args) {
+
+        car car1=new car();
+
+
+        //we need to set speed value
+        car1.speed = 101;
+        car1.run();
+
+    }
+
+    public void run() {
+        System.out.println("car is runnig at "+speed);
+    }
 }
