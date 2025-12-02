d28d5e9560af01e2fed3491c1d320aeab8c92b73
Mksa2130 <mksa2130@gmail.com>
2025-12-02 10:29:29 +0530

    main

--- src/oop/course.java
+++ src/oop/course.java
@@ -1,4 +1,25 @@
 package oop;
 
 public class course {
+    String name;
+     int duration;
+
+     course()
+     {
+         System.out.println("constructer called...");
+     }
+
+    public static void main(String[] args) {
+        System.out.println("before object create ");
+
+        course c= new course();
+        c.learn();
+        course c2=new course();
+
+
+    }
+    void learn()
+    {
+        System.out.println();
+    }
 }
