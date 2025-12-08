b50722389faa1807684cbf869abe2d20a030b87c
Mksa2130 <mksa2130@gmail.com>
2025-12-08 14:21:38 +0530

    main

---
+++ src/String/StringDemoP01/StringDemoP01.java
@@ -0,0 +1,10 @@
+package String.StringDemoP01;
+
+public class StringDemo01 {
+    public static void main(String[] args) {
+        String s1= new String("Mrityunjay");
+        String s2 ="Mrityunjay";
+        System.out.println(s1.hashCode());
+
+    }
+}
