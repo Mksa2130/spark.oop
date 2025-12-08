f2961259b418a204a4c9a1fa29e7f396fb0597b7
Mksa2130 <mksa2130@gmail.com>
2025-12-08 14:26:32 +0530

    Update StringDemoP01

--- src/String/StringDemoP01/StringDemoP01.java
+++ src/String/StringDemoP01/StringDemoP01.java
@@ -1,10 +1,14 @@
 package String.StringDemoP01;
 
-public class StringDemo01 {
+public class StringDemoP01 {
     public static void main(String[] args) {
         String s1= new String("Mrityunjay");
         String s2 ="Mrityunjay";
+        String s3="Mrityunjay";
+//        String s4=new String("Mrityunjay")
         System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+        System.out.println(s3.hashCode());
 
     }
 }
