783ba8a8bd9884d297aaa07c76ec1065d9870b9a
Mksa2130 <mksa2130@gmail.com>
2025-12-10 15:26:27 +0530

    main

--- src/String/String/vowels.java
+++ src/String/String/vowels.java
@@ -1,19 +1,25 @@
 package String.String;
 
 public class vowels {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
-        String s1 = "Mrityunjay";
+        String s1 = "mrityunjay";
         int v=0; int c=0;
         for (char c1:s1.toCharArray())
         {
-            if ("aeiou")
+            if ("aeiou".indexOf(c1)!=-1)
             {
                 v++;
             }
-            else{
+            else
+            {
                 c++;
             }
         }
+        System.out.println("vowel count = "+v);
+        System.out.println("consonant cout = "+c);
+
+
     }
 }
