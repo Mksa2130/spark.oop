7d3b62d19c9feef698018034fed21b448aa82d2c
Mksa2130 <mksa2130@gmail.com>
2026-02-24 20:08:20 +0530

    ain

---
+++ src/Multythreding/MainTheardDemo.java
@@ -0,0 +1,7 @@
+package Multythreding;
+
+public class MainTheardDemo {
+    public static void main(String[] args) {
+        System.out.println("Thread name "+Thread.currentThread().getName());
+    }
+}
--- src/Multythreding/ThredDemo.java
+++ src/Multythreding/ThredDemo.java
@@ -1,4 +1,21 @@
 package Multythreding;
-
+class Mythread extends Thread
+{
+    @Override
+    public void run() {
+        for(int i=0;i<6; i++)
+        {
+            System.out.println("Child thread");
+        }
+    }
+}
 public class ThredDemo {
+    public static void main(String[] args) {
+        Mythread t =new Mythread();
+        t.start();
+        for (int i=0; i<6;i++){
+            System.out.println("main thread");
+        }
+
+    }
 }
